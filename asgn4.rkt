fb5a740c29cd036cb0df349df43930aee6812c9d
Joseph Dewar <superjoeyd123@gmail.com>
2023-04-28 14:50:55 -0700

    test cases

--- asgn4.rkt
+++ asgn4.rkt
@@ -109,3 +109,82 @@
     [(leq0? test then else) (leq0? (subst x v test) (subst x v then) (subst x v else))]
     [(FunAppC fun arg) (FunAppC fun (subst x v arg))]))
 
+;; Test cases for parsing
+(define a1 (BinopC '+ (NumC 1) (NumC 2)))
+(define a2 (BinopC '+ (NumC 3) a1))
+(define a3 (BinopC '* a1 a2))
+(define sub (BinopC '- (NumC 3) (NumC 2)))
+(define div (BinopC '/ (NumC 4) (NumC 2)))
+(define leq0-1 (leq0? (NumC 1) a1 a2))
+(define leq0-2 (leq0? (NumC -1) a1 a2))
+(check-equal? (parse '(+ 1 2)) a1)
+(check-equal? (parse '(+ 3 (+ 1 2))) a2)
+(check-equal? (parse '(* (+ 1 2) (+ 3 (+ 1 2)))) a3)
+(check-equal? (parse '(* 3 (- 1 2))) (BinopC '* (NumC 3) (BinopC '- (NumC 1) (NumC 2))))
+(check-equal? (parse '(- 3 2)) sub)
+(check-equal? (parse '(/ 4 2)) div)
+(check-exn #rx"expression" (lambda () (parse '{+ 4})))
+(check-exn #rx"operator" (lambda () (parse '{& 4 5})))
+(check-exn #rx"VVQS: illegal expression" (lambda () (parse 'def)))
+(check-equal? (parse '(leq0? 1 then (+ 1 2) else (+ 3 (+ 1 2)))) leq0-1)
+(check-equal? (parse '(leq0? 1 then 2 else 3)) (leq0? (NumC 1) (NumC 2) (NumC 3)))
+(check-equal? (parse 'x) (IdC 'x))
+(check-equal? (parse '(f 5)) (FunAppC 'f (NumC 5)))
+
+;; Test cases for function parsing
+(check-equal? (parse-fundef '{def {add x} = {+ x 1}}) (FunC 'add 'x (BinopC '+ (IdC 'x) (NumC 1))))
+(check-equal? (parse-fundef '{def {mul x} = {* x 2}}) (FunC 'mul 'x (BinopC '* (IdC 'x) (NumC 2))))
+(check-exn #rx"illegal function" (lambda () (parse-fundef '(def (def x) = (+ x 1)))))
+
+;; Test cases for program parsing and interpretation
+(define test-prog1
+  '{{def {add x} = {+ x 1}}
+    {def {mul x} = {* x 2}}
+    {def {main init} = {add {mul init}}}})
+
+(define test-prog2
+  '{{def {sub x} = {- x 1}}
+    {def {div x} = {/ x 2}}
+    {def {main init} = {sub {div init}}}})
+
+(check-equal? (top-interp test-prog1) 1) ; (add (mul 0)) = (add 0) = 1
+(check-equal? (top-interp test-prog2) -1) ; (sub ((div 0)) = (sub 0) = -1
+
+;; Test cases for conditional expressions
+(define test-prog3
+'{{def {negate x} = {leq0? x then {- 0 x} else {* -1 x}}}
+{def {main init} = {negate {+ init 1}}}})
+
+(check-equal? (top-interp test-prog3) -1) ; (negate (+ 0 1)) = (negate 1) = -1
+
+;; Test case that reaches the "empty? funs" branch in lookup-fun
+(define test-prog-no-main
+'{{def {add x} = {+ x 1}}
+{def {mul x} = {* x 2}}})
+
+(check-exn #rx"VVQS: function not found" (lambda () (top-interp test-prog-no-main)))
+
+;; Test case that reaches the "else (lookup-fun name (cdr funs))" branch in lookup-fun
+(define test-prog-multiple-funs
+'{{def {add x} = {+ x 1}}
+{def {mul x} = {* x 2}}
+{def {main init} = {mul init}}})
+
+(check-equal? (top-interp test-prog-multiple-funs) 0) ; (mul 0) = 0
+
+;; Test case to reach unbound identifier
+(define test-prog-unbound-id
+'{{def {main init} = unbound-id}})
+
+(check-exn #rx"unbound identifier" (lambda () (top-interp test-prog-unbound-id)))
+
+;; Single interp test cases (no functions)
+(check-equal? (interp a1 '()) 3)
+(check-equal? (interp a2 '()) 6)
+(check-equal? (interp a3 '()) 18)
+(check-equal? (interp sub '()) 1)
+(check-equal? (interp div '()) 2)
+(check-equal? (interp (NumC 0) '()) 0)
+(check-exn #rx"zero" (λ () (interp (BinopC '/ (NumC 5) (NumC 0)) '() )))
+(check-equal? (interp leq0-1 '()) 6)
+(check-equal? (interp leq0-2 '()) 3)
